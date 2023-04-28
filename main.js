6d23968f30dbaae754904a66ed3d93851219eccf
yazed <s201730270@kfupm.edu.sa>
2023-04-28 13:23:44 +0300

    - Playing with textures and assigning them to different map types

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import * as THREE from 'three';
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
-import {TextureLoader} from "three";
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 5
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x8ebbd3)
+
+const light = new THREE.AmbientLight(0xffffff)
+scene.add(light)
+
 const renderer = new THREE.WebGLRenderer({
     antialias: true,
 })
@@ -16,13 +19,19 @@ const controls = new OrbitControls(camera, renderer.domElement)
 const stats = new Stats()
 document.body.appendChild(stats.domElement)
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const boxMaterial = new THREE.MeshBasicMaterial({
-
+const boxGeometry = new THREE.BoxGeometry()
+const boxMaterial = new THREE.MeshStandardMaterial({
+    // color:0xf800d3
 })
 const textureLoader = new THREE.TextureLoader()
-const texture = textureLoader.load('/assets/textures/uv-test-bw.png',)
-boxMaterial.map = texture
+const bwTexture = textureLoader.load('/assets/textures/uv-test-bw.png',)
+const colTexture = textureLoader.load('/assets/textures/uv-test-col.png')
+boxMaterial.alphaMap = bwTexture
+boxMaterial.transparent = true
+// boxMaterial.aoMap = colTexture
+// boxMaterial.displacementMap = bwTexture
+// boxMaterial.map = bwTexture
+
 
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box)
@@ -34,4 +43,5 @@ function animate() {
     stats.update()
     renderer.render(scene, camera)
 }
+    console.log('textures:', bwTexture, colTexture)
 animate()
